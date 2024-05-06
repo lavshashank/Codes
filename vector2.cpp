77da395297dfd75d4ee54cb563add51fd7a9f6e4
Lav Shashank <lavshashank431@gmail.com>
2024-05-06 17:26:12 +0530

    06-05-2024

---
+++ vector2.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main(){
+
+    vector<int> v;
+
+    //for loop
+
+    for (int i = 0; i < 5; i++)
+    {
+        int element;
+        cin>>element;
+        v.push_back(element);
+    }
+    
+for (int i = 0; i < v.size(); i++)
+{
+    cout<<v[i]<<" ";
+}
+cout<<endl;
+
+
+//for each loop
+for(int ele:v){
+    cout<<ele<<" ";
+}
+cout<<endl;
+}
