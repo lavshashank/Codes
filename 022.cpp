1a108e374be97dd7cc86892fb606eb5f63bd2e47
Lav Shashank <lavshashank431@gmail.com>
2024-07-26 00:27:56 +0530

    DSA

--- 022.cpp
+++ 022.cpp
@@ -7,7 +7,7 @@ void print(vector<int> &v){
     for (int i = 0; i < v.size(); i++)
     {
         cout<<v[i]<<" ";
-    }    
+    }
 }
 
 int main(){
---
+++ 023.cpp
@@ -0,0 +1,9 @@
+// Given an integer array 'a' sorted in non-decreasing order, return an array of the squares of each number sorted in non-decreasing order.
+
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main(){
+    
+}
